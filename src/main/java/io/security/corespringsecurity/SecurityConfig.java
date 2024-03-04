027d69a1d279d07df546e9f91e1a589281583e6a
tngh4037 <tngh4037@gmail.com>
2024-03-05 00:27:46 +0900

    feat: 섹션1의 6)내용

--- src/main/java/io/security/corespringsecurity/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/SecurityConfig.java
@@ -1,16 +1,19 @@
 package io.security.corespringsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -18,41 +21,62 @@ import java.io.IOException;
 @EnableWebSecurity // 웹 보안을 활성화 하기 위한 annotation
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     /**
      * (인증 / 인가) API를 추가적으로 설정할 수 있는 configure 메서드를 오버라이딩한다.
      * 그러면 configure 메서드 에서 설정한 API 설정 정보로 웹 보안이 활성화 된다.
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 인가 정책
         http
                 .authorizeRequests()
-                .anyRequest().authenticated();
-        // 인증 정책
-        http
-                .formLogin();
-        // 로그아웃 정책
-        http
-                .logout() // 이하의 코드가 로그아웃에 관한 설정임을 명시
-                .logoutUrl("/logout") // 로그아웃 요청 POST url (default: /logout)
-                .logoutSuccessUrl("/login") // 로그아웃 성공 시 이동할 url
-                .addLogoutHandler(new LogoutHandler() { // 로그아웃 처리 핸들러
+                .antMatchers("/loginPage").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login_proc")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error=true")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .successHandler(new AuthenticationSuccessHandler() {
                     @Override
-                    public void logout(HttpServletRequest request,
-                                       HttpServletResponse response,
-                                       Authentication authentication) {
-                        HttpSession session = request.getSession();
-                        session.invalidate();
+                    public void onAuthenticationSuccess(HttpServletRequest request,
+                                                        HttpServletResponse response,
+                                                        Authentication authentication) throws IOException {
+                        System.out.println("success");
+                        response.sendRedirect("/");
                     }
                 })
-                .logoutSuccessHandler(new LogoutSuccessHandler() { // 로그아웃 성공 시 핸들러
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request,
+                                                        HttpServletResponse response,
+                                                        AuthenticationException e) throws IOException {
+                        System.out.println("failure");
+                        response.sendRedirect("/login");
+                    }
+                })
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
                     @Override
                     public void onLogoutSuccess(HttpServletRequest request,
                                                 HttpServletResponse response,
                                                 Authentication authentication) throws IOException {
+                        System.out.println("logout");
                         response.sendRedirect("/login");
                     }
                 })
-                .deleteCookies("remember-me"); // 로그아웃 후 쿠키 삭제
+                .and()
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService)
+        ;
     }
 }
