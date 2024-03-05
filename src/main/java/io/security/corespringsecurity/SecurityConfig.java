77ca5b8ad31bc2f94ffd3d32c7f7d7b5552bddfb
tngh4037 <tngh4037@gmail.com>
2024-03-06 05:27:29 +0900

    feat: 섹션1의 9)내용

--- src/main/java/io/security/corespringsecurity/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/SecurityConfig.java
@@ -1,28 +1,13 @@
 package io.security.corespringsecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity // 웹 보안을 활성화 하기 위한 annotation
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    UserDetailsService userDetailsService;
 
     /**
      * (인증 / 인가) API를 추가적으로 설정할 수 있는 configure 메서드를 오버라이딩한다.
@@ -36,8 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .formLogin();
         http
-                .rememberMe()
-                .userDetailsService(userDetailsService)
-        ;
+                .sessionManagement()
+                .sessionFixation().changeSessionId();
     }
 }
