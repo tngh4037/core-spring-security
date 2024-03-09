a445faf168098907b5ce01aa81d0b6d9d6f295b5
tngh4037 <tngh4037@gmail.com>
2024-03-10 04:16:56 +0900

    섹션1의 12)내용

--- src/main/java/io/security/corespringsecurity/SecurityController.java
+++ src/main/java/io/security/corespringsecurity/SecurityController.java
@@ -26,4 +26,13 @@ public class SecurityController {
         return "admin";
     }
 
+    @GetMapping("/denied")
+    public String denied() {
+        return "Access is denied";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
