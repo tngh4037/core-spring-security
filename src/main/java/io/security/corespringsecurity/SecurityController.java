e9d94f5e396b9441d7dfccc98c79da81b027e724
tngh4037 <tngh4037@gmail.com>
2024-03-15 10:03:01 +0900

    섹션2의 9)내용

--- src/main/java/io/security/corespringsecurity/SecurityController.java
+++ src/main/java/io/security/corespringsecurity/SecurityController.java
@@ -1,43 +1,19 @@
 package io.security.corespringsecurity;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index(HttpSession session) {
-
-        // 1) SecurityContext 에 저장된 인증 객체 조회
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        // 2) 세션에 저장된 인증 객체 조회
-        SecurityContext context = (SecurityContext) session.getAttribute(
-                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
-        Authentication authentication1 = context.getAuthentication();
-
+    public String index() {
         return "home";
     }
 
-    @GetMapping("/thread")
-    public String thread() {
-        new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-                    }
-                }
-        ).start();
-
-        return "thread";
+    @GetMapping("/user")
+    public String user() {
+        return "user";
     }
 
 }
