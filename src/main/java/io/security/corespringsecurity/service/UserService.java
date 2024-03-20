488266e6fca157c7255e4ff829b5184bec04f7cd
tngh4037 <tngh4037@gmail.com>
2024-03-20 13:24:27 +0900

    feat: section3-1 의 내용

--- src/main/java/io/security/corespringsecurity/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package io.security.corespringsecurity.service;
-
-import io.security.corespringsecurity.domain.Account;
-
-public interface UserService {
-
-    void createUser(Account account);
-
-}
